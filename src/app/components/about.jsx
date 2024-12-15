376a5d5cc294e850ffd2ee87271ea473eece7f04
shprince1 <princehsylvanus@gmail.com>
2024-12-15 23:22:29 +0100

    text completed

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -108,8 +108,9 @@ const About = () => {
 
             <p>
               As a trusted partner for corporate organizations in Nigeria, we
-              are committed to delivering innovative digital experiences.
-              {/* drive growth and efficiency. Our team comprises seasoned
+              are committed to delivering innovative digital experiences that
+              drive growth and efficiency.
+              {/* Our team comprises seasoned
               technology enthusiasts and practitioners who are passionate about
               leveraging technology to craft sustainable and impactful solutions
               for businesses. */}
