8a0fe1904e048e58bc63b2c392e3206bc87e1d4a
shprince1 <princehsylvanus@gmail.com>
2024-12-22 07:12:29 +0100

    image comented

--- src/app/components/about-us/card.jsx
+++ src/app/components/about-us/card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import style from "./styles/card.module.scss";
 // import Images from '../about-us/images/abt-banner.jpg'
 // import { GrFormNextLink } from "react-icons/gr";
