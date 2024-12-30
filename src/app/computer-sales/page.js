c67bfe7442e76f23193881c13d9f232fe7ea2aaa
shprince1 <princehsylvanus@gmail.com>
2024-12-31 00:41:27 +0100

    pages added

--- src/app/computer-sales/page.js
+++ src/app/computer-sales/page.js
@@ -11,17 +11,17 @@ const ComputerSales = () => {
         image1:repair,
     //   title: "IT Consulting & Advisory",
       head: "COMPUTER SALES AND MAINTENANCE",
-      titledetails: "At Essage Technologies, we specialize in delivering custom software solutions tailored to your unique business needs. Our expertise spans the entire software development lifecycle, from ideation and design to deployment and maintenance. We leverage modern technologies and agile methodologies to create efficient, scalable, and user-friendly applications. Whether it’s a web app, mobile solution, or enterprise software, our goal is to empower your business with innovative tools that drive productivity and growth. Partner with us to transform your ideas into powerful software solutions that deliver real value.",
+      titledetails: "At EssAge Technologies, we specialize in providing high-quality computers and peripherals in both large quantities and retail. Whether you’re an individual seeking a personal system or a company outfitting an entire office, we have tailored solutions to meet your needs.",
       
-      subtitle:'Software Development Process with EssAge Technologies',
-      description: "Requirement Analysis and Planning",
-      contentdetails:" We begin by understanding your business objectives and gathering detailed requirements. Through consultations and workshops, we define the project scope, timelines, and deliverables, ensuring alignment with your vision.",
-      description2: "Design and Prototyping",
-      contentdetails2:"Our team creates intuitive designs and prototypes, focusing on user experience and functionality. Feedback loops during this phase ensure that the solution meets your expectations before development begins.",
-      description3: "Development and Testing",
-      contentdetails3:" Using agile methodologies, we build the software in iterative cycles, ensuring flexibility and timely updates. Rigorous testing at every stage guarantees performance, security, and reliability.",
-      description4:"Deployment and Maintenance",
-      contentdetails4:"Once approved, the software is deployed seamlessly into your environment. We provide ongoing support, updates, and enhancements to ensure the solution remains effective and adaptable to your evolving needs."
+      subtitle:'COMPUTER SALES AND MAINTENANCE',
+      description: "Bulk and Retail Sales",
+      contentdetails:" We supply top-tier computers and hardware, offering competitive pricing for bulk orders and a wide selection for retail customers. Our products are sourced from trusted brands to ensure reliability and performance.",
+      description2: "Maintenance Services",
+      contentdetails2:"Our expert team offers comprehensive maintenance and support, ensuring your systems operate at peak efficiency. From routine servicing to urgent repairs, we’ve got you covered. With a commitment to quality and customer satisfaction, we’re your trusted partner for all computer sales and maintenance needs.",
+      // description3: "Development and Testing",
+      // contentdetails3:" Using agile methodologies, we build the software in iterative cycles, ensuring flexibility and timely updates. Rigorous testing at every stage guarantees performance, security, and reliability.",
+      // description4:"Deployment and Maintenance",
+      // contentdetails4:"Once approved, the software is deployed seamlessly into your environment. We provide ongoing support, updates, and enhancements to ensure the solution remains effective and adaptable to your evolving needs."
     }
 
 
@@ -44,10 +44,10 @@ const ComputerSales = () => {
             description={data.description}
             contentdetails2={data.contentdetails2}
             description2={data.description2}
-            contentdetails3={data.contentdetails3}
-            description3={data.description3}
-            contentdetails4={data.contentdetails4}
-            description4={data.description4}
+            // contentdetails3={data.contentdetails3}
+            // description3={data.description3}
+            // contentdetails4={data.contentdetails4}
+            // description4={data.description4}
           
           />
           );
--- src/app/solar-inverter/page.js
+++ src/app/solar-inverter/page.js
@@ -1,58 +1,55 @@
 import React from "react";
 // import DetailedServiceCard from "../components/detailed-service components/detailed-service-card";
 import DetailedCardBanner from "../components/detailed-service components/detailed-card-banner";
-
+import Solar from '../solar-inverter/solar.jpg'
+import Solar2 from '../solar-inverter/solar2.jpg'
 const SolarInverter = () => {
   const data = [
     {
-        intro:"IT Consulting & Advisory",
+        intro:"Solar Inverter Installation Services",
     //   title: "IT Consulting & Advisory",
-      head: "IT Consulting & Advisory",
-      titledetails:
-        " We have developed several web-based and mobile solutions for a  wide variety of clients from different commercial and public sectors. Tell us about your business requirements in the form below",
+    image1:Solar,
+    image:Solar2,
+      head: "Solar Inverter Installation Services",
+      titledetails: "At EssAge Technologies, we provide professional solar inverter installation services designed to maximize energy efficiency and sustainability. Our team specializes in the installation of reliable and high-performance solar inverters tailored to meet residential, commercial, and industrial energy needs. From system design and installation to testing and maintenance, we ensure seamless integration with your solar power systems for optimal performance. With a focus on quality, safety, and customer satisfaction, we help you harness the power of the sun to reduce energy costs and support a greener future.",
       
-      subtitle:'A Structured Consultancy process with EssAge Technologies ',
-      description: "IT Consulting & Advisory",
-      contentdetails:" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vita voluptas! Laboriosam sunt mollitia explicabo cumque illo? Repellendus earum est reiciendis. Placeat quasi dignissimos fugiat, voluptatem libero reprehenderit incidunt adipisci doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ipsa.",
+    subtitle:'Our Solar Inverter Installation Process',
+    description: "Consultation and Site Assessment",
+    contentdetails:" We begin with a detailed consultation to understand your energy requirements. Our team conducts a site assessment to evaluate factors like space, sunlight availability, and existing electrical systems.",
+    description2: "Customized System Design",
+    contentdetails2:"Based on the assessment, we design a solar inverter solution optimized for your specific needs. We ensure the system is scalable, efficient, and compliant with industry stand",
+    description3: "Installation and Integration",
+    contentdetails3:" Our skilled technicians handle the installation process with precision, ensuring seamless integration with your solar panels and electrical infrastructure. Safety and quality are our top priorities.",
+    description4:"Testing, Handover, and Support",
+    contentdetails4:"Once installed, we thoroughly test the system to confirm functionality and efficiency. After handover, we provide user training, detailed documentation, and ongoing support to ensure your system performs optimally for years to come. With EssAge Technologies, transitioning to solar energy is simple, reliable, and rewarding. Let us help you achieve sustainable energy goals today!",
     },
 
-    // {
-    //   title: "COD TRAVELS",
-    //   description: "Software Development",
-    //   details: "We develop software solutions accross various industries. Which enhance smooth and efficienntrunning of day to day business",
-
-    //   roleplayed: "Designed the frontend user-interface",
-
-    // },
-    // {
-    //   title: "Bankers Ware house",
-    //   description: "LAN Networking & CCTV Installation ",
-    //   details: "Our LAN networking and CCTV security camera installation, is sure to deliver seamless connectivity and top-notch survelance for your home or business",
-
-    // },
-    // {
-    //   head: "Bankers Ware house",
-    //   description: "Computer Sale & Maintenance",
-    //   details: "We sell computers in bulk or single units, and also provide professional maintenance and repair services to keep your systems running smoothly",
-
-    // },
+   
   ];
 
   return (
     <div>
       <div>
-        {data.map((data, index) => {
+      {data.map((data, index) => {
           return (
             <DetailedCardBanner
-              key={index}
-              intro={data.intro}
-              titledetails={data.titledetails}
-              subtitle={data.subtitle}
-              image={data.image}
-              head={data.head}
-              contentdetails={data.contentdetails}
-              description={data.description}
-            />
+            key={index}
+            intro={data.intro}
+            titledetails={data.titledetails}
+            subtitle={data.subtitle}
+            image={data.image}
+            image1={data.image1}
+            head={data.head}
+            contentdetails={data.contentdetails}
+            description={data.description}
+            contentdetails2={data.contentdetails2}
+            description2={data.description2}
+            contentdetails3={data.contentdetails3}
+            description3={data.description3}
+            contentdetails4={data.contentdetails4}
+            description4={data.description4}
+          
+          />
           );
         })}
       </div>
